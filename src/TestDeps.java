95414baf55bb5ec662c8654bf7ffe4a68a7b170d
Marissa K Wilson <Marissa@MarissaKWilson.com>
2014-04-16 16:39:26 -0400

    update gitignore, add TestDeps

---
+++ src/TestDeps.java
@@ -0,0 +1,13 @@
+import org.apache.commons.logging.impl.Log4JLogger;
+
+import edu.uci.ics.jung.graph.UndirectedSparseGraph;
+
+
+public class TestDeps {
+public static void main(String[] args) {
+	new Log4JLogger();
+	UndirectedSparseGraph<Integer, Integer> graph;
+	
+	
+}
+}
