72e8300dcd27fd920ac0bcca9d3a99a2a3b85c43
Marissa K Wilson <Marissa@MarissaKWilson.com>
2014-04-24 12:40:56 -0400

    Added to sprial class, added ColorAssigner class

---
+++ src/display/ColorAssigner.java
@@ -0,0 +1,9 @@
+package display;
+/**
+ * Assigns color to glyphs based on their proximity to different authors
+ * @author M
+ *
+ */
+public class ColorAssigner {
+
+}
--- src/display/Spiral.java
+++ src/display/Spiral.java
@@ -6,6 +6,7 @@ import graphmap.Glyph;
  * The spiral algorithm
  * Gets a starting point, iteratively spirals until intersect algorithm 
  * returns false. (Does it intersect? Yes? Keep going. No? Place)
+ * Stretch goal: Weight spiral towards more dominant author
  * @author M
  *
  */
