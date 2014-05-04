40dfec36372a3f30137256b871665c12c0313a44
Marissa K Wilson <Marissa@MarissaKWilson.com>
2014-05-03 23:26:39 -0400

    Added Main class. All other changes made during meeting

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+/**
+ * The main method
+ * Executes the main design of the program
+ * @author M
+ *
+ */
+public class Main {
+	
+	public static void main(String[] args){
+		int prevDays;
+	
+		System.out.println("How many days would you like commits from?");
+		Scanner scan = new Scanner(System.in);
+		prevDays = scan.nextInt();
+	
+	}
+}
