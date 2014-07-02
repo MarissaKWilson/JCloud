66f539d2c04051ac78624c93e0e54913e79a54d5
unknown <Marissa@MarissaKWilson.com>
2014-07-02 13:39:49 -0400

    Added hashcode to SourceCodeFile

--- src/graphmap/SourceCodeFile.java
+++ src/graphmap/SourceCodeFile.java
@@ -46,9 +46,9 @@ public class SourceCodeFile {
 		glyphs.remove(g);
 	}
 	
-	public Author getAuthor(){
+	public List getAuthors(){
 		System.out.println("SourceCodeFile: return stored author");
-		return author;
+		return authors;
 	}
 	
 	public void setAuthor(Author a){
@@ -56,12 +56,37 @@ public class SourceCodeFile {
 		authors.add(a);
 	}
 	
-	public void setDiffed(List<String> tokens){
+	public void setDiffed(LinkedList<String> tokens){
 		diffedTokens = tokens;
 	}
 	
 	public List getTokens(){
 		return diffedTokens;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((c == null) ? 0 : c.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SourceCodeFile other = (SourceCodeFile) obj;
+		if (c == null) {
+			if (other.c != null)
+				return false;
+		} else if (!c.equals(other.c))
+			return false;
+		return true;
 	}
 	
 	//TODO hashcode
